f3f8e526a9dbb2a1870764b42957c1bc67eaaf86
Long Hoang <long@mindworker.de>
2016-09-23 13:36:35 +0800

    all: fix golint and go vet issues (#34)
    Signed-off-by: Long Hoang <long@mindworker.de>

--- condition_string_equal.go
+++ condition_string_equal.go
@@ -1,12 +1,13 @@
 package ladon
 
-// SubjectIsOwnerCondition is a condition which is fulfilled if the subject of a warden request is also the owner
-// of the requested resource.
+// StringEqualCondition is a condition which is fulfilled if the given
+// string value is the same as specified in StringEqualCondition
 type StringEqualCondition struct {
 	Equals string `json:"equals"`
 }
 
-// Fulfills returns true if the the request is fulfilled by the condition.
+// Fulfills returns true if the given value is a string and is the
+// same as in StringEqualCondition.Equals
 func (c *StringEqualCondition) Fulfills(value interface{}, _ *Request) bool {
 	s, ok := value.(string)
 
