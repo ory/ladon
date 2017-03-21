49ec4c6d0b03fec03e70dd2cde85b8134c99f8f6
Ben Krieger <bkrieger@smartperimeter.io>
2017-03-21 10:03:45 -0400

    Move go:generate for mock

--- manager/manager.go
+++ manager/manager.go
@@ -2,6 +2,8 @@ package manager
 
 import "github.com/ory/ladon/policy"
 
+//go:generate mockgen -package access_test -destination ../access/manager_mock_test.go github.com/ory/ladon/manager Manager
+
 // Manager is responsible for managing and persisting policies.
 type Manager interface {
 
