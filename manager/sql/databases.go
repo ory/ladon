8c5de2c8320ce03fbb5c1380cc84f33f0e32584c
Jason Hutchinson <jhutchinson@wehco.com>
2017-11-07 14:51:23 -0600

    combine shared migrations

--- manager/sql/databases.go
+++ manager/sql/databases.go
@@ -14,90 +14,95 @@ type Statements struct {
 	FindRequestCandidates         string
 }
 
+var sharedMigrations = []*migrate.Migration{
+	{
+		Id: "1",
+		Up: []string{
+			`CREATE TABLE IF NOT EXISTS ladon_policy (
+				id           varchar(255) NOT NULL PRIMARY KEY,
+				description  text NOT NULL,
+				effect       text NOT NULL CHECK (effect='allow' OR effect='deny'),
+				conditions	 text NOT NULL
+			)`,
+			`CREATE TABLE IF NOT EXISTS ladon_policy_subject (
+				compiled text NOT NULL,
+				template varchar(1023) NOT NULL,
+				policy   varchar(255) NOT NULL,
+				FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE
+			)`,
+			`CREATE TABLE IF NOT EXISTS ladon_policy_permission (
+				compiled text NOT NULL,
+				template varchar(1023) NOT NULL,
+				policy   varchar(255) NOT NULL,
+				FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE
+			)`,
+			`CREATE TABLE IF NOT EXISTS ladon_policy_resource (
+				compiled text NOT NULL,
+				template varchar(1023) NOT NULL,
+				policy   varchar(255) NOT NULL,
+				FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE
+			)`,
+		},
+		Down: []string{
+			"DROP TABLE ladon_policy",
+			"DROP TABLE ladon_policy_subject",
+			"DROP TABLE ladon_policy_permission",
+			"DROP TABLE ladon_policy_resource",
+		},
+	},
+	{
+		Id: "2",
+		Up: []string{
+			`CREATE TABLE IF NOT EXISTS ladon_subject (
+				id          varchar(64) NOT NULL PRIMARY KEY,
+				has_regex   bool NOT NULL,
+				compiled    varchar(511) NOT NULL UNIQUE,
+				template    varchar(511) NOT NULL UNIQUE
+			)`,
+			`CREATE TABLE IF NOT EXISTS ladon_action (
+				id          varchar(64) NOT NULL PRIMARY KEY,
+				has_regex   bool NOT NULL,
+				compiled    varchar(511) NOT NULL UNIQUE,
+				template    varchar(511) NOT NULL UNIQUE
+			)`,
+			`CREATE TABLE IF NOT EXISTS ladon_resource (
+				id          varchar(64) NOT NULL PRIMARY KEY,
+				has_regex   bool NOT NULL,
+				compiled    varchar(511) NOT NULL UNIQUE,
+				template    varchar(511) NOT NULL UNIQUE
+			)`,
+			`CREATE TABLE IF NOT EXISTS ladon_policy_subject_rel (
+				policy   varchar(255) NOT NULL,
+				subject  varchar(64) NOT NULL,
+				PRIMARY KEY (policy, subject),
+				FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE,
+				FOREIGN KEY (subject) REFERENCES ladon_subject(id) ON DELETE CASCADE
+			)`,
+			`CREATE TABLE IF NOT EXISTS ladon_policy_action_rel (
+				policy  varchar(255) NOT NULL,
+				action  varchar(64) NOT NULL,
+				PRIMARY KEY (policy, action),
+				FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE,
+				FOREIGN KEY (action) REFERENCES ladon_action(id) ON DELETE CASCADE
+			)`,
+			`CREATE TABLE IF NOT EXISTS ladon_policy_resource_rel (
+				policy    varchar(255) NOT NULL,
+				resource  varchar(64) NOT NULL,
+				PRIMARY KEY (policy, resource),
+				FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE,
+				FOREIGN KEY (resource) REFERENCES ladon_resource(id) ON DELETE CASCADE
+			)`,
+		},
+		Down: []string{},
+	},
+}
+
 var Databases = map[string]Statements{
 	"postgres": {
 		Migrations: &migrate.MemoryMigrationSource{
 			Migrations: []*migrate.Migration{
-				{
-					Id: "1",
-					Up: []string{
-						`CREATE TABLE IF NOT EXISTS ladon_policy (
-							id           varchar(255) NOT NULL PRIMARY KEY,
-							description  text NOT NULL,
-							effect       text NOT NULL CHECK (effect='allow' OR effect='deny'),
-							conditions	 text NOT NULL
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_subject (
-							compiled text NOT NULL,
-							template varchar(1023) NOT NULL,
-							policy   varchar(255) NOT NULL,
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_permission (
-							compiled text NOT NULL,
-							template varchar(1023) NOT NULL,
-							policy   varchar(255) NOT NULL,
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_resource (
-							compiled text NOT NULL,
-							template varchar(1023) NOT NULL,
-							policy   varchar(255) NOT NULL,
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE
-						)`,
-					},
-					Down: []string{
-						"DROP TABLE ladon_policy",
-						"DROP TABLE ladon_policy_subject",
-						"DROP TABLE ladon_policy_permission",
-						"DROP TABLE ladon_policy_resource",
-					},
-				},
-				{
-					Id: "2",
-					Up: []string{
-						`CREATE TABLE IF NOT EXISTS ladon_subject (
-							id          varchar(64) NOT NULL PRIMARY KEY,
-							has_regex   bool NOT NULL,
-							compiled    varchar(511) NOT NULL UNIQUE,
-							template    varchar(511) NOT NULL UNIQUE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_action (
-							id          varchar(64) NOT NULL PRIMARY KEY,
-							has_regex   bool NOT NULL,
-							compiled    varchar(511) NOT NULL UNIQUE,
-							template    varchar(511) NOT NULL UNIQUE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_resource (
-							id          varchar(64) NOT NULL PRIMARY KEY,
-							has_regex   bool NOT NULL,
-							compiled    varchar(511) NOT NULL UNIQUE,
-							template    varchar(511) NOT NULL UNIQUE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_subject_rel (
-							policy   varchar(255) NOT NULL,
-							subject  varchar(64) NOT NULL,
-							PRIMARY KEY (policy, subject),
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE,
-							FOREIGN KEY (subject) REFERENCES ladon_subject(id) ON DELETE CASCADE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_action_rel (
-							policy  varchar(255) NOT NULL,
-							action  varchar(64) NOT NULL,
-							PRIMARY KEY (policy, action),
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE,
-							FOREIGN KEY (action) REFERENCES ladon_action(id) ON DELETE CASCADE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_resource_rel (
-							policy    varchar(255) NOT NULL,
-							resource  varchar(64) NOT NULL,
-							PRIMARY KEY (policy, resource),
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE,
-							FOREIGN KEY (resource) REFERENCES ladon_resource(id) ON DELETE CASCADE
-						)`,
-					},
-					Down: []string{},
-				},
+				sharedMigrations[0],
+				sharedMigrations[1],
 				{
 					Id: "3",
 					Up: []string{
@@ -147,86 +152,8 @@ var Databases = map[string]Statements{
 	"mysql": {
 		Migrations: &migrate.MemoryMigrationSource{
 			Migrations: []*migrate.Migration{
-				{
-					Id: "1",
-					Up: []string{
-						`CREATE TABLE IF NOT EXISTS ladon_policy (
-							id           varchar(255) NOT NULL PRIMARY KEY,
-							description  text NOT NULL,
-							effect       text NOT NULL CHECK (effect='allow' OR effect='deny'),
-							conditions	 text NOT NULL
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_subject (
-							compiled text NOT NULL,
-							template varchar(1023) NOT NULL,
-							policy   varchar(255) NOT NULL,
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_permission (
-							compiled text NOT NULL,
-							template varchar(1023) NOT NULL,
-							policy   varchar(255) NOT NULL,
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_resource (
-							compiled text NOT NULL,
-							template varchar(1023) NOT NULL,
-							policy   varchar(255) NOT NULL,
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE
-						)`,
-					},
-					Down: []string{
-						"DROP TABLE ladon_policy",
-						"DROP TABLE ladon_policy_subject",
-						"DROP TABLE ladon_policy_permission",
-						"DROP TABLE ladon_policy_resource",
-					},
-				},
-				{
-					Id: "2",
-					Up: []string{
-						`CREATE TABLE IF NOT EXISTS ladon_subject (
-							id          varchar(64) NOT NULL PRIMARY KEY,
-							has_regex   bool NOT NULL,
-							compiled    varchar(511) NOT NULL UNIQUE,
-							template    varchar(511) NOT NULL UNIQUE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_action (
-							id          varchar(64) NOT NULL PRIMARY KEY,
-							has_regex   bool NOT NULL,
-							compiled    varchar(511) NOT NULL UNIQUE,
-							template    varchar(511) NOT NULL UNIQUE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_resource (
-							id          varchar(64) NOT NULL PRIMARY KEY,
-							has_regex   bool NOT NULL,
-							compiled    varchar(511) NOT NULL UNIQUE,
-							template    varchar(511) NOT NULL UNIQUE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_subject_rel (
-							policy   varchar(255) NOT NULL,
-							subject  varchar(64) NOT NULL,
-							PRIMARY KEY (policy, subject),
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE,
-							FOREIGN KEY (subject) REFERENCES ladon_subject(id) ON DELETE CASCADE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_action_rel (
-							policy  varchar(255) NOT NULL,
-							action  varchar(64) NOT NULL,
-							PRIMARY KEY (policy, action),
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE,
-							FOREIGN KEY (action) REFERENCES ladon_action(id) ON DELETE CASCADE
-						)`,
-						`CREATE TABLE IF NOT EXISTS ladon_policy_resource_rel (
-							policy    varchar(255) NOT NULL,
-							resource  varchar(64) NOT NULL,
-							PRIMARY KEY (policy, resource),
-							FOREIGN KEY (policy) REFERENCES ladon_policy(id) ON DELETE CASCADE,
-							FOREIGN KEY (resource) REFERENCES ladon_resource(id) ON DELETE CASCADE
-						)`,
-					},
-					Down: []string{},
-				},
+				sharedMigrations[0],
+				sharedMigrations[1],
 				{
 					Id: "3",
 					Up: []string{
