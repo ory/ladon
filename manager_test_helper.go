4526b47ef41947d05e9a3043fdfd4fdc128848ed
Adhithya Ravindra <AdhithyaKuthethur.Ravindra@tesco.com>
2018-02-06 18:04:18 +0530

    [WorkInProgress] Added length assert
    Signed-off-by: Adhithya Ravindra <Adhithya.Ravindra@gmail.com>

--- manager_test_helper.go
+++ manager_test_helper.go
@@ -372,6 +372,7 @@ func TestHelperCreateGetDelete(s Manager) func(t *testing.T) {
 			assert.Len(t, pols3, 0)
 
 			found := map[string]int{}
+			assert.Len(t, found, len(TestManagerPolicies))
 			for _, got := range pols {
 				for _, expect := range TestManagerPolicies {
 					//This is a modified equality check
@@ -382,7 +383,7 @@ func TestHelperCreateGetDelete(s Manager) func(t *testing.T) {
 			}
 
 			for _, f := range found {
-				//This assert is supposed to fail
+				//This assert is supposed to pass
 				assert.Equal(t, 1, f)
 			}
 		})
