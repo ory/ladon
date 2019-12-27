3260fb4acdcdfbb8647bd92526590a9d11b196bd
RafPe <rafal@pieniazek.nl>
2019-12-27 15:13:07 +0100

    adds per metric function description

--- ladon.go
+++ ladon.go
@@ -26,9 +26,13 @@ import (
 
 // Metric is used to expose metrics about authz
 type Metric interface {
+	// RequestDeniedBy is called when we get explicit deny by policy
 	RequestDeniedBy(Request, Policy)
+	// RequestAllowedBy is called when a matching policy has been found.
 	RequestAllowedBy(Request, Policies)
+	// RequestNoMatch is called when no policy has matched our request
 	RequestNoMatch(Request)
+	// RequestProcessingError is called when unexpected error occured
 	RequestProcessingError(Request, Policy, error)
 }
 
