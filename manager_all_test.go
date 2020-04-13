5e7b85d3352156fde7363b12c3beb979e8d70862
yukels <dmitry.yukelson@gmail.com>
2020-04-13 17:08:40 +0300

    fix: "import the testing package in the manager_test_helper.go"

--- manager_all_test.go
+++ manager_all_test.go
@@ -41,13 +41,13 @@ func connectMEM() {
 func TestManagers(t *testing.T) {
 	t.Run("type=get errors", func(t *testing.T) {
 		for k, s := range managers {
-			t.Run("manager="+k, TestHelperGetErrors(s))
+			t.Run("manager="+k, testHelperGetErrors(s))
 		}
 	})
 
 	t.Run("type=CRUD", func(t *testing.T) {
 		for k, s := range managers {
-			t.Run(fmt.Sprintf("manager=%s", k), TestHelperCreateGetDelete(s))
+			t.Run(fmt.Sprintf("manager=%s", k), testHelperCreateGetDelete(s))
 		}
 	})
 
@@ -56,8 +56,8 @@ func TestManagers(t *testing.T) {
 			"postgres": managers["postgres"],
 			"mysql":    managers["mysql"],
 		} {
-			t.Run(fmt.Sprintf("manager=%s", k), TestHelperFindPoliciesForSubject(k, s))
-			t.Run(fmt.Sprintf("manager=%s", k), TestHelperFindPoliciesForResource(k, s))
+			t.Run(fmt.Sprintf("manager=%s", k), testHelperFindPoliciesForSubject(k, s))
+			t.Run(fmt.Sprintf("manager=%s", k), testHelperFindPoliciesForResource(k, s))
 		}
 	})
 }
